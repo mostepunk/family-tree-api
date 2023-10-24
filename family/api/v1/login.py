978419e42c8eb674c371d1046e7122c09218f239
mostepan.vi <fan.42@mail.ru>
2023-10-24 23:01:51 +0300

    dependency injection only where it needs

--- family/api/v1/login.py
+++ family/api/v1/login.py
@@ -18,18 +18,12 @@ async def login_user(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     service: AccountService = Depends(Provide[Container.account_service]),
 ):
-    # service = AccountService(AccountUOW())
     user = await service.authentiacate_user(form_data.username, form_data.password)
     return {"access_token": await create_token(user), "token_type": "bearer"}
 
 
 @login.get("/refresh-token", summary="Обновить токен без аутентификации")
-@inject
-async def refresh_token(
-    account: Annotated[get_user_from_token, Depends()],
-    service: AccountService = Depends(Provide[Container.account_service]),
-):
-    # service = AccountService(AccountUOW())
+async def refresh_token(account: Annotated[get_user_from_token, Depends()]):
     return {"access_token": await create_token(account), "token_type": "bearer"}
 
 
@@ -38,11 +32,7 @@ async def refresh_token(
     summary="information about authorised user",
     response_model=AccountSchema,
 )
-@inject
-async def about_me(
-    account: Annotated[get_user_from_token, Depends()],
-    service: AccountService = Depends(Provide[Container.account_service]),
-):
+async def about_me(account: Annotated[get_user_from_token, Depends()]):
     return account
 
 
--- family/utils/container.py
+++ family/utils/container.py
@@ -4,13 +4,6 @@ from family.adapters.db import Database
 from family.services.accounts import AccountService, AccountUOW
 from family.settings import db_settings
 
-# from src.adapters.clients.aiohttp_client import AiohttpClient
-# from src.adapters.clients.grpc.blog_client import BlogClient
-# from src.adapters.db.database import Database
-# from src.service_layer.publication_service import PublicationService
-# from src.service_layer.unit_of_work.publication_uow import PublicationUnitOfWork
-# from src.settings import db_settings
-
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
